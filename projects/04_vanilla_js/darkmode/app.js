74d6250decf38ebdcccd163c472d8284629d93e8
rishi_chauhan <rishisingh99314@gmail.com>
2024-01-07 00:55:26 +0530

    first commit

--- projects/04_vanilla_js/darkmode/app.js
+++
@@ -1,6 +0,0 @@
-const body = document.body;
-const darkModeToggle = document.getElementById("darkModeToggle");
-
-darkModeToggle.addEventListener('click', function() {
-    body.classList.toggle("dark-mode");
-});
--- projects/05_a_react/minimal/src/app.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-    return < h1 > Hello World < /h1>;
-};
-
-ReactDOM.render( < App / > , document.getElementById('root'));
